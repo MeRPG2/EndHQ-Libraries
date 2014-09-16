48ccde00ff284b383c0e24f40a7ed000cc443387
MeRPG <jaxon007@verizon.net>
2014-09-15 20:53:19 -0400

    Crude RemoteEntities Implementation

--- src/net/endhq/bukkitplugin/BukkitPlugin.java
+++ src/net/endhq/bukkitplugin/BukkitPlugin.java
@@ -3,12 +3,20 @@ package net.endhq.bukkitplugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import de.kumpelblase2.remoteentities.RemoteEntities;
+
 public class BukkitPlugin extends JavaPlugin {
 	private static BukkitPlugin inst;
+	RemoteEntities reme;
 	@Override
 	public void onEnable() {
 		this.getLogger().info("Woo! EndHQ's Libraries have loaded! :D");
 		inst=this;
+		reme=new RemoteEntities();
+	}
+	@Override
+	public void onDisable() {
+		reme.onDisable();
 	}
 	public static BukkitPlugin getInst() {
 		return inst;
