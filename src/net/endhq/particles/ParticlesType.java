89a8e7df18e58cdbe809a30ed3967c9a36ce4506
MeRPG <jaxon007@verizon.net>
2014-09-17 07:17:29 -0400

    Attemted to version proof it.

--- src/net/endhq/particles/ParticlesType.java
+++ src/net/endhq/particles/ParticlesType.java
@@ -1,5 +1,7 @@
 package net.endhq.particles;
 
+import java.lang.reflect.Field;
+
 import net.minecraft.server.v1_7_R4.PacketPlayOutWorldParticles;
 
 import org.bukkit.Location;
@@ -49,7 +51,35 @@ public enum ParticlesType {
 	}
 	
 	public void sendToPlayer(Player player, Location location, float offsetX, float offsetY, float offsetZ, float speed, int count) throws Exception {
-		PacketPlayOutWorldParticles packet = new PacketPlayOutWorldParticles();
+		Class packetClass=null;
+		for(int v = 1; v<9; v++) {
+			for(int r = 1; r<10; r++) {
+				try {
+					packetClass = Class.forName("net.minecraft.server.v1_"+v+"_R"+r+".PacketPlayOutWorldParticles");
+					break;
+				} catch(Exception ex) {
+					continue;
+				}
+			}
+		}
+		Class playerClass = null;
+		for(int v = 1; v<9; v++) {
+			for(int r = 1; r<10; r++) {
+				try {
+					playerClass = Class.forName("net.minecraft.server.v1_"+v+"_R"+r+".entity.CraftPlayer");
+					break;
+				} catch(Exception ex) {
+					continue;
+				}
+			}
+		}
+		if(packetClass==null) {
+			throw new ClassNotFoundException("Could not find packet class.");
+		}
+		if(playerClass==null) {
+			throw new ClassNotFoundException("Could not find player class.");
+		}
+		Object packet = Class.forName("net.minecraft.server").newInstance();
 	    ReflectionUtil.setValue(packet, "a", this.particleName);
 	    ReflectionUtil.setValue(packet, "b", Float.valueOf((float)location.getX()));
 	    ReflectionUtil.setValue(packet, "c", Float.valueOf((float)location.getY()));
@@ -59,6 +89,9 @@ public enum ParticlesType {
 	    ReflectionUtil.setValue(packet, "g", Float.valueOf(offsetZ));
 	    ReflectionUtil.setValue(packet, "h", Float.valueOf(speed));
 	    ReflectionUtil.setValue(packet, "i", Integer.valueOf(count));
-	    ((CraftPlayer)player).getHandle().playerConnection.sendPacket(packet);
+	    Object cPlayer = playerClass.cast(player);
+	    Field pCon = cPlayer.getClass().getMethod("getHandle", null).invoke(cPlayer, null).getClass().getField("playerConnection");
+	    pCon.get(cPlayer).getClass().getMethod("sendPacket", packet.getClass()).invoke(pCon, packet);//.getHandle().playerConnection.sendPacket(packet);
+	    
 	}
 }
